e54b0ce05f49009efc476881dbf4713a9b64fa8d
Mark Brown <MarkBrown@fusable.com>
2026-09-22 09:21:17 -0500

    Move Palmer API credentials to GitHub Actions secrets

--- generate_google_feed.py
+++ generate_google_feed.py
@@ -11,8 +11,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = "https://prod-palmertrucks-inventory-ws-ti0xfp.5sc6y6-4.usa-e2.cloudhub.io/api/inventory"
 headers = {
-    "client_id": "8d6377ba1b17482393c269defe61da54",
-    "client_secret": "736cfa551eA1495bba3a0020A604dfF7",
+    "client_id": os.environ["PALMER_API_CLIENT_ID"],
+    "client_secret": os.environ["PALMER_API_CLIENT_SECRET"],
     "Accept": "application/json"
 }
 
